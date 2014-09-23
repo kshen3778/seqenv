dcbadc051c702f0159cfd76cc200c52e535835d4
Lucas Sinclair <lucas.sinclair@me.com>
2014-09-23 09:06:57 +0200

    Added a main python module called 'seqenv'

---
+++ seqenv/__init__.py
@@ -0,0 +1,15 @@
+b'This module needs Python 2.7.x'
+
+# Special variables #
+__version__ = '0.9.0'
+
+# Built-in modules #
+
+# Internal modules #
+
+# Third party modules #
+
+################################################################################
+class Analysis(object):
+    """The main object."""
+    pass
