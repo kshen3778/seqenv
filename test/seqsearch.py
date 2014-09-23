bcb5249e83e43c73bc380d86d8f2a1410dd1fb7c
Lucas Sinclair <lucas.sinclair@me.com>
2014-09-23 17:56:41 +0300

    Added a test script for the sequence search code

---
+++ test/seqsearch.py
@@ -0,0 +1,19 @@
+"""
+==================
+Test the SeqSearch
+==================
+"""
+
+# Modules #
+import os, inspect
+from seqenv.fasta import FASTA
+from seqenv.seqsearch.parallel import ParallelSeqSearch
+
+# Constants #
+current_script = inspect.getframeinfo(inspect.currentframe()).filename
+current_dir = os.path.dirname(os.path.abspath(current_script)) + '/'
+fasta = FASTA(current_dir + "community.fasta")
+
+################################################################################
+search = ParallelSeqSearch(fasta, 'nucl', 'nt', algorithm='blast', num_threads=3)
+search.run()
