af9bb023e87a3524c6d342cd3a918037218e91c9
Lucas Sinclair <lucas.sinclair@me.com>
2014-09-24 13:17:12 +0300

    Added a second test script

---
+++ test/pipeline.py
@@ -0,0 +1,18 @@
+"""
+=======================
+Test the whole pipeline
+=======================
+"""
+
+# Modules #
+import os, inspect
+from seqenv import Analysis
+
+# Constants #
+current_script = inspect.getframeinfo(inspect.currentframe()).filename
+current_dir = os.path.dirname(os.path.abspath(current_script)) + '/'
+fasta = current_dir + "community.fasta"
+
+################################################################################
+analysis = Analysis(fasta, num_threads=3, min_identity=0.1, e_value=0.1)
+print analysis.gi_to_text
