a771d3dbbb2011890525b418246fc7540f8acbfe
Lucas Sinclair <lucas.sinclair@me.com>
2015-12-30 18:45:15 +0100

    Rename the tagger test because you can't import tagger

--- tests/tagger_test.py
+++ tests/tagger_test.py
@@ -10,7 +10,7 @@ import tagger, os
 # Script #
 t = tagger.Tagger()
 home = os.environ['HOME'] + '/'
-data_dir = home + "repos/seqenv/data/"
+data_dir = "../seqenv/data_envo/"
 t.LoadNames(data_dir + 'envo_entities.tsv', data_dir + 'envo_names.tsv')
 t.LoadGlobal(data_dir + 'envo_global.tsv')
 t.GetMatches("oceanic soil", "", [-27])
