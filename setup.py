8518ad929ee0e30ad9db02c34140841e909ade29
Lucas Sinclair <lucas.sinclair@me.com>
2016-04-25 13:46:58 +0200

    "Version 1.1.3"

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name             = 'seqenv',
-    version          = '1.1.2',
+    version          = '1.1.3',
     description      = 'Assign environment ontology (EnvO) terms to short DNA sequences',
     license          = 'MIT',
     url              = 'https://github.com/xapple/seqenv',
-    download_url     = 'https://github.com/xapple/seqenv/tarball/1.1.2',
+    download_url     = 'https://github.com/xapple/seqenv/tarball/1.1.3',
     author           = 'Lucas Sinclair',
     author_email     = 'lucas.sinclair@me.com',
     classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics'],
